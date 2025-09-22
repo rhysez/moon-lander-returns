2371da35a6d25ab755a22da5cd8c8a94542f9c89
Rhys <121772484+rhysez@users.noreply.github.com>
2025-09-22 21:17:56 +0100

    Replace legacy lander design with new version

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,17 @@
 extends RigidBody2D
 
-const TORQUE: float = 100
+const TORQUE: float = 20
 const ROTATION_SPEED: float = 80
-var rotation_direction: int = 0
+var rotation_direction: float = 0
 var thrust = Vector2(0, -450)
 
-# Lander should move in the direction it is facing on thrust action.
-# Lander should tilt right and left on tilt_right and tilt_left actions
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	if Input.is_action_pressed("thrust"):
 		state.apply_force(thrust.rotated(rotation))
 	
 	if Input.is_action_pressed("tilt_right"):
-		rotation_direction += 1
+		rotation_direction += 2
 	if Input.is_action_pressed("tilt_left"):
-		rotation_direction -= 1
+		rotation_direction -= 2
 	
 	state.apply_torque(rotation_direction * TORQUE)
