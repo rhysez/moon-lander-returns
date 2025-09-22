dd370846417e60859128ed2e254e9a47cbae163d
Rhys <121772484+rhysez@users.noreply.github.com>
2025-09-22 20:22:59 +0100

    Refactor script to use _integrate_forces method for thrusting lander upwards

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,14 @@
 extends RigidBody2D
 
-const MAX_MOVEMENT_SPEED: int = 400
+const TORQUE: float = 20000
 const ROTATION_SPEED: float = 1.5
-
 var rotation_direction: int = 0
-
-func get_input() -> void:
-	rotation_direction = Input.get_axis("tilt_left", "tilt_right")
-	# velocity = transform.y * Input.get_axis("thrust", "") * MAX_MOVEMENT_SPEED
+var thrust = Vector2(0, -450)
 
 # Lander should move in the direction it is facing on thrust action.
 # Lander should tilt right and left on tilt_right and tilt_left actions
-func _physics_process(delta: float) -> void:
-	get_input()
-	rotation += rotation_direction * ROTATION_SPEED * delta
-	# move_and_slide()
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if Input.is_action_pressed("thrust"):
+		state.apply_force(thrust.rotated(rotation))
+	
+	rotation_direction = Input.get_axis("tilt_left", "tilt_right")
